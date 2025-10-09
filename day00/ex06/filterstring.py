ef5dc7dadef7480c74220fbb2b23ef8d673ed845
Rania Daoudi Idrissi <rania@MacBook-Pro-de-Rania.local>
2025-10-09 11:44:19 +0200

    add ex06

---
+++ day00/ex06/filterstring.py
@@ -0,0 +1,22 @@
+import sys
+from ft_filter import ft_filter
+
+
+def main(argv):
+	try:
+		assert len(argv) == 3, "the arguments are bad"
+		s = argv[1]
+		try:
+			n = int(argv[2])
+		except ValueError:
+			raise AssertionError("the arguments are bad")
+		words = s.split()
+		result = list(ft_filter(lambda w: len(w) > n, words))
+		print(result)
+	except AssertionError as e:
+		print(f"AssertionError: {e}")
+		sys.exit(1)
+
+
+if __name__ == "__main__":
+	main(sys.argv)
